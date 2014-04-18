cdc192555c58ee4b42946fe61914aea3f750589e
Ziming Wei <weiziming1989@gmail.com>
2014-04-18 15:13:43 -0700

    Remove Duplicates from Sorted Array

---
+++ remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (A[i] != A[index]) {
+                index++;
+                A[index] = A[i];
+            }
+        }
+        return index + 1;
+    }
+};
