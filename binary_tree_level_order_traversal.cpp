73abe68b904a4e0e46ea280ad311e4a04d8ecb85
Ziming Wei <weiziming1989@gmail.com>
2014-05-02 11:32:30 -0700

    Binary Tree Level Order Traversal

---
+++ binary_tree_level_order_traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Time: O(n)
+    // Space: O(n)
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        vector<vector<int> > result;
+        if (root == NULL) {
+            return result;
+        }
+        
+        queue<TreeNode*> curr, next;
+        curr.push(root);
+        while (!curr.empty()) {
+            vector<int> level;
+            while (!curr.empty()) {
+                TreeNode *node = curr.front();
+                curr.pop();
+                level.push_back(node->val);
+                if (node->left != NULL) {
+                    next.push(node->left);
+                }
+                if (node->right != NULL) {
+                    next.push(node->right);
+                }
+            }
+            result.push_back(level);
+            swap(curr, next);
+        }
+        return result;
+    }
+};
