57940a850c307873f4d291e069cbe4bd78d05982
Ziming Wei <weiziming1989@gmail.com>
2014-05-06 15:56:47 -0700

    Set Matrix Zeroes

---
+++ set_matrix_zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // Time: O(m*n)
+    // Space: O(m+n)
+    void setZeroes(vector<vector<int> > &matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (row[i]) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (col[j]) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
