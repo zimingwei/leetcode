de3acaf156e2633e51cfbae6e25e8b36373e2e46
Ziming Wei <weiziming1989@gmail.com>
2014-05-06 16:14:37 -0700

    Update set_matrix_zeroes.cpp

--- set_matrix_zeroes.cpp
+++ set_matrix_zeroes.cpp
@@ -31,3 +31,50 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    // Time: O(m*n)
+    // Space: O(1)
+    void setZeroes(vector<vector<int> > &matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+        bool first_row = false;
+        bool first_col = false;
+        
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                first_col = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                first_row = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (first_row) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (first_col) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
