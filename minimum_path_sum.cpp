d2293ce1f516b4f2d411fb15bd1fa66b8e2013d4
Ziming Wei <weiziming1989@gmail.com>
2014-05-06 14:07:27 -0700

    Minimum Path Sum

---
+++ minimum_path_sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // Time: O(mn)
+    // Space: O(mn)
+    // Let f[i][j] be the minimum path sum from (0, 0) to (i, j)
+    // f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+    int minPathSum(vector<vector<int> > &grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int> > f(m, vector<int>(n, 0));
+        
+        f[0][0] = grid[0][0];
+        for (int j = 1; j < n; j++) { // first row
+            f[0][j] = f[0][j-1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) { // first column
+            f[i][0] = f[i-1][0] + grid[i][0];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j];
+            }
+        }
+        return f[m-1][n-1];
+    }
+};
