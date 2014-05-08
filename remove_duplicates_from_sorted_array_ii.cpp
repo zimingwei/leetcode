5522faab33ddb555668e1b4cd6249a2e05f1f0be
Ziming Wei <weiziming1989@gmail.com>
2014-05-07 17:31:53 -0700

    Update remove_duplicates_from_sorted_array_ii.cpp

--- remove_duplicates_from_sorted_array_ii.cpp
+++ remove_duplicates_from_sorted_array_ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
+    // Time: O(n)
+    // Space: O(1)
     int removeDuplicates(int A[], int n) {
         if (n <= 2) {
             return n;
         }
-        
         int index = 2;
         for (int i = 2; i < n; i++) {
             if (A[i] != A[index-2]) {
-                A[index] = A[i];
-                index++;
+                A[index++] = A[i];
             }
         }
         return index;
