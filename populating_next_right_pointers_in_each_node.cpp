24030f8004f913c329f158264cd20196d2f06069
Ziming Wei <weiziming1989@gmail.com>
2014-05-08 14:07:06 -0700

    Update populating_next_right_pointers_in_each_node.cpp

--- populating_next_right_pointers_in_each_node.cpp
+++ populating_next_right_pointers_in_each_node.cpp
@@ -28,3 +28,35 @@ public:
         connect(root, NULL);
     }
 };
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Time: O(n)
+    // Space: O(1)
+    void connect(TreeLinkNode *root) {
+        if (root == NULL) {
+            return;
+        }
+        root->next = NULL;
+        TreeLinkNode *prev = root; // previous level
+        while (prev != NULL && prev->left != NULL) {
+            TreeLinkNode *next = prev->left; // next level
+            while (prev != NULL) {
+                TreeLinkNode *curr = prev->left;
+                curr->next = prev->right;
+                curr = curr->next;
+                prev = prev->next;
+                curr->next = (prev == NULL) ? NULL : prev->left;
+            }
+            prev = next;
+        }
+    }
+};
