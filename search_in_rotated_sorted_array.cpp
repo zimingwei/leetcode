3ae9881dd3dd8f0c5d18cc8760422875c2670379
Ziming Wei <weiziming1989@gmail.com>
2014-05-09 19:58:15 -0700

    Update search_in_rotated_sorted_array.cpp

--- search_in_rotated_sorted_array.cpp
+++ search_in_rotated_sorted_array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Time: O(logn)
+    // Space: O(1)
     int search(int A[], int n, int target) {
         int left = 0;
         int right = n - 1;
@@ -8,14 +10,14 @@ public:
             if (A[mid] == target) {
                 return mid;
             } 
-            if (A[mid] >= A[left]) { // A[mid] is greater than or equal to A[left]
-                if (target >= A[left] && target < A[mid]) { // target is less than A[mid]
+            if (A[mid] >= A[left]) {
+                if (target >= A[left] && target < A[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                if (target > A[mid] && target <= A[right]) { // target is greater than A[mid]
+                if (target > A[mid] && target <= A[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
