6479f1d4c1e1dcfb91487af2c230bddda58a56ab
Ziming Wei <weiziming1989@gmail.com>
2014-05-12 17:08:00 -0700

    Update subsets.cpp

--- subsets.cpp
+++ subsets.cpp
@@ -24,3 +24,27 @@ public:
         return result;
     }
 };
+
+class Solution {
+private:
+    void DFS(vector<int> &S, int start, vector<int> &subset,
+             vector<vector<int> > &result) {
+        result.push_back(subset);
+        
+        for (int i = start; i < S.size(); i++) {
+            subset.push_back(S[i]);
+            DFS(S, i+1, subset, result);
+            subset.pop_back();
+        }
+    }
+public:
+    // Time: O(2^n)
+    // Space: O(n)
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<vector<int> > result;
+        vector<int> subset;
+        DFS(S, 0, subset, result);
+        return result;
+    }
+};
