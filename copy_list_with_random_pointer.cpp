b907ae680001ea21756a6c4fda75d18e60afa09b
Ziming Wei <weiziming1989@gmail.com>
2014-06-02 17:46:57 -0700

    Update copy_list_with_random_pointer.cpp

--- copy_list_with_random_pointer.cpp
+++ copy_list_with_random_pointer.cpp
@@ -27,3 +27,44 @@ public:
         return mapping[head];
     }
 };
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Time: O(n)
+    // Space: O(1)
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        if (head == NULL) {
+            return NULL;
+        }
+        
+        for (RandomListNode *curr = head; curr != NULL; ) {
+            RandomListNode *node = new RandomListNode(curr->label);
+            node->next = curr->next;
+            curr->next = node;
+            curr = node->next;
+        }
+        for (RandomListNode *curr = head; curr != NULL; 
+                curr = curr->next->next) {
+            if (curr->random != NULL) {
+                curr->next->random = curr->random->next;
+            }
+        }
+        RandomListNode helper(0);
+        for (RandomListNode *curr = head, *newCurr = &helper;
+                curr != NULL; ) {
+            newCurr->next = curr->next;
+            newCurr = newCurr->next;
+            curr->next = curr->next->next;
+            curr = curr->next;
+        }
+        return helper.next;
+    }
+};
